fc2fc85c88a2865c197b54f01b069f1dab2ca14a
Ilia Fedin <infedinwork@gmail.com>
2026-05-15 11:19:51 +0300

    feat(infra): setup EF Core and db configuration

---
+++ src/AuctionSystem.Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AuctionSystem.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace AuctionSystem.Infrastructure.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
