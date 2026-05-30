ec12fc5644d3a20eab4d6d90297967739bc8c360
Ilia Fedin <infedinwork@gmail.com>
2026-05-30 17:50:49 +0300

    feat(presentation): add auth endpoints
    - Implement IEndpoint interface for endpoint definitions
    - Create EndpointExtensions for automatic endpoint registration in DI
    - Add Register and Login endpoints (minimal api)

---
+++ src/AuctionSystem.Presentation/Endpoints/Users/LoginEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using AuctionSystem.Application.Features.Users.Login;
+using AuctionSystem.Presentation.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuctionSystem.Presentation.Endpoints.Users
+{
+    public sealed class LoginEndpoint : IEndpoint
+    {
+        public sealed record Request(
+            [Required, EmailAddress]
+            string Email,
+            [Required]
+            string Password);
+
+        public sealed record Response(string Token);
+
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("api/users/login", (Request request, ISender sender, CancellationToken cancellationToken) 
+                => Login(request, sender, cancellationToken));
+        }
+
+        private static async Task<IResult> Login(Request request, ISender sender, CancellationToken cancellationToken)
+        {
+            var command = new LoginUserCommand(request.Email, request.Password);
+
+            var result = await sender.Send(command, cancellationToken);
+
+            if (result.IsSuccess)
+                return Results.Ok(new Response(Token: result.Value));
+
+            return CustomResults.Problem(result);
+        }
+    }
+}
---
+++ src/AuctionSystem.Presentation/Extensions/EndpointExtensions.cs
@@ -0,0 +1,36 @@
+﻿using AuctionSystem.Presentation.Endpoints;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AuctionSystem.Presentation.Extensions
+{
+    public static class EndpointExtensions
+    {
+        public static IServiceCollection AddEndpoints(this IServiceCollection services)
+        {
+            // Find all non-abstract classes implementing IEndpoint in the current assembly
+            var endpointTypes = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.IsClass &&        // Must be a class
+                           !t.IsAbstract &&     // // Must be not abstract
+                           typeof(IEndpoint).IsAssignableFrom(t)); // Must implement IEndpoint
+
+            foreach (var type in endpointTypes)
+            {
+                services.AddSingleton(typeof(IEndpoint), type);
+            }
+
+            return services;
+        }
+        public static void MapEndpoints(this WebApplication app)
+        {
+            var endpoints = app.Services.GetServices<IEndpoint>();
+
+            foreach (var endpoint in endpoints)
+            {
+                endpoint.MapEndpoint(app);
+            }
+        }
+    }
+}
