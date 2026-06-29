f827c91808083123743d25632d989b119e6a2d7c
Ilia Fedin <infedinwork@gmail.com>
2026-06-29 16:44:55 +0300

    feat(auctions): implement close auction feature

---
+++ src/AuctionSystem.Application/Features/Auctions/Commands/Cancel/CancelAuctionCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AuctionSystem.Application.Abstractions;
+using AuctionSystem.Domain.Auctions;
+using AuctionSystem.Domain.Primitives;
+using AuctionSystem.Domain.Security;
+using MediatR;
+
+namespace AuctionSystem.Application.Features.Auctions.Commands.Cancel
+{
+    public sealed class CancelAuctionCommandHandler(
+        IAuctionRepository auctionRepository,
+        IClock clock,
+        ICurrentUserProvider currentUserProvider,
+        IUnitOfWork uow
+        ) : IRequestHandler<CancelAuctionCommand, Result>
+    {
+        public async Task<Result> Handle(CancelAuctionCommand request, CancellationToken cancellationToken)
+        {
+            if (!AuctionId.TryParse(request.AuctionId, out var auctionId))
+                return Result.Failure(AuctionErrors.InvalidAuctionId());
+
+            var userId = currentUserProvider.UserId;
+            if (userId is null)
+                return Result.Failure(SecurityErrors.Unauthorized());
+
+            var auction = await auctionRepository.GetByIdAsync(auctionId, cancellationToken);
+            if (auction is null)
+                return Result.Failure(AuctionErrors.NotFound(auctionId));
+
+            if (userId != auction.SellerId)
+                return Result.Failure(SecurityErrors.Forbidden());
+
+            var now = clock.UtcNow;
+
+            var cancelResult = auction.Cancel(now);
+            if (cancelResult.IsFailure)
+                return cancelResult;
+
+            await uow.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
--- src/AuctionSystem.Domain/Auctions/AuctionStatus.cs
+++ src/AuctionSystem.Domain/Auctions/AuctionStatus.cs
@@ -4,6 +4,7 @@
     {
         Scheduled = 0,
         Active = 1,
-        Closed = 2
+        Closed = 2,
+        Cancelled = 3
     }
 }
