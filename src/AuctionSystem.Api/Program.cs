852f21f097b33b803641a45eb1269c5904e2da93
Ilia Fedin <infedinwork@gmail.com>
2026-09-12 12:43:02 +0300

    feat(docker): add apply migrations in pipeline and health checks with db check

--- src/AuctionSystem.Api/Program.cs
+++ src/AuctionSystem.Api/Program.cs
@@ -1,8 +1,8 @@
+using AuctionSystem.Api.Extensions;
 using AuctionSystem.Application;
 using AuctionSystem.Infrastructure;
 using AuctionSystem.Presentation;
 using AuctionSystem.Presentation.Extensions;
-using AuctionSystem.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +13,11 @@ builder.Services
 
 builder.Services.AddSwaggerDocumentation();
 builder.Services.AddAuthorization();
+builder.Services.AddHealthChecks(builder.Configuration);
 
 var app = builder.Build();
+
+await app.ApplyMigrationsAsync();
 
 app.UseSwaggerDocumentation();
 
